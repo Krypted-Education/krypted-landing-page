40f4c01bcfaedf5366cd59deb628405c47ae146e
msusur <mail@mertsusur.com>
2017-12-26 16:45:44 +0000

    Initial commit for node app

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+  app = new express(),
+  minify = require('express-minify');
+
+app.set('port', (process.env.PORT || 5000));
+
+if (!process.env.DEBUG) {
+  app.use(minify({
+    cache: true
+  }));
+}
+app.use("/", express.static("Coming-soon-page"));
+// app.use("/vendor", express.static("bower_components"));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
